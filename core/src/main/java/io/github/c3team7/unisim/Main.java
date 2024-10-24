9c182a86a81ebceaada28400b4391a5fe884fae6
VolcanicSpectre <sl12doc@gmail.com>
2024-10-24 14:48:42 +0100

    Testing grid sizes

--- core/src/main/java/io/github/c3team7/unisim/Main.java
+++ core/src/main/java/io/github/c3team7/unisim/Main.java
@@ -2,33 +2,40 @@ package io.github.c3team7.unisim;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    private ShapeRenderer shapeRenderer;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+
+        System.out.println(Gdx.graphics.getHeight());
+        int tileSize = 20;
+        for (int y=0; y<Gdx.graphics.getHeight(); y = y + tileSize){
+            for (int x=0; x<Gdx.graphics.getWidth(); x = x + tileSize) {
+                shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+                shapeRenderer.setColor((x % (2 * tileSize) != y % (2 * tileSize)) ? Color.GREEN: Color.RED);
+                shapeRenderer.rect(x, y, tileSize, tileSize);
+                shapeRenderer.end();
+            }
+        }
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        shapeRenderer.dispose();
     }
 }
