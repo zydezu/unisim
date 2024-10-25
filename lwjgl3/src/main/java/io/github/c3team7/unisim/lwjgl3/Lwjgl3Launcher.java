5883dbaa3b2ae992f77a79269a9cbcfe248ba8c8
zydezu <kinomaxxing@gmail.com>
2024-10-25 13:35:55 +0100

    test viewport sizes

--- lwjgl3/src/main/java/io/github/c3team7/unisim/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/c3team7/unisim/lwjgl3/Lwjgl3Launcher.java
@@ -28,6 +28,7 @@ public class Lwjgl3Launcher {
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
         configuration.setWindowedMode(1280, 720);
+        configuration.setWindowSizeLimits(640, 360, 1920, 1080);
         //// You can change these files; they are in lwjgl3/src/main/resources/ .
         configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
         return configuration;
