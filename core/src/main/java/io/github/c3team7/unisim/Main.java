dbc9e7a4000247f15ffc15f574379e98e5053e08
zydezu <kinomaxxing@gmail.com>
2024-10-25 14:27:20 +0100

    improve performance by getting resolution on create(), this also fixes window resizing

--- core/src/main/java/io/github/c3team7/unisim/Main.java
+++ core/src/main/java/io/github/c3team7/unisim/Main.java
@@ -3,11 +3,13 @@ package io.github.c3team7.unisim;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FillViewport;
@@ -18,46 +20,62 @@ import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class Main extends Game {
+    private OrthographicCamera gameCamera;
+    private OrthographicCamera hudCamera;
     private ShapeRenderer shapeRenderer;
+
+    private Vector2 playerPosition = new Vector2();
+    private boolean cameraFollowsPlayer = true;
 
     private SpriteBatch spriteBatch;
     private BitmapFont font;
 
+    ScreenInfo screeninfo;
+
+    class ScreenInfo {
+        public ScreenInfo(int width, int height, int refresh) {
+            this.width = width;
+            this.height = height;
+            this.refresh = refresh;
+        }
+
+        public String toString() {
+            return String.format("Resolution: %1$s x %2$s @ %3$s Hz", width, height, refresh); // string formatting
+        }
+
+        int width;
+        int height;
+        int refresh;
+    }
+
     @Override
     public void create() {
-        shapeRenderer = new ShapeRenderer();
+        float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
+        float viewableWorldWidth = 32.0f;
+        gameCamera = new OrthographicCamera(viewableWorldWidth, viewableWorldWidth * aspectRatio);
+        gameCamera.position.set(playerPosition.x, playerPosition.y, 1.0f);
+
+        hudCamera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        hudCamera.position.set(hudCamera.viewportWidth / 2.0f, hudCamera.viewportHeight / 2.0f, 1.0f);
 
         shapeRenderer = new ShapeRenderer();
         spriteBatch = new SpriteBatch();
 
-        font = new BitmapFont(Gdx.files.internal("default.fnt"));
+        font = new BitmapFont(Gdx.files.internal("default.fnt"));    
+
+        screeninfo = new ScreenInfo(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), Gdx.graphics.getDisplayMode().refreshRate);
+        System.out.println(screeninfo);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        class ScreenInfo {
-            public ScreenInfo(int width, int height, int refresh) {
-                this.width = width;
-                this.height = height;
-                this.refresh = refresh;
-            }
-
-            public String toString() {
-                return String.format("Resolution: %1$s x %2$s @ %3$s Hz", width, height, refresh); // string formatting
-            }
-
-            int width;
-            int height;
-            int refresh;
-        }
-
-        ScreenInfo screeninfo = new ScreenInfo(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), Gdx.graphics.getDisplayMode().refreshRate);
-        System.out.println(screeninfo);
+        gameCamera.update();
 
         int tileSize = 20;
-        for (int y = 0; y < screeninfo.width; y = y + tileSize) {
+        for (int y = 0; y < screeninfo.height; y = y + tileSize) {
             for (int x = 0; x < screeninfo.width; x = x + tileSize) {
                 shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
                 shapeRenderer.setColor((x % (2 * tileSize) != y % (2 * tileSize)) ? Color.PURPLE : Color.BLUE);
@@ -67,7 +85,7 @@ public class Main extends Game {
         }
 
         spriteBatch.begin();
-        font.draw(spriteBatch, "FPS:" + + Gdx.graphics.getFramesPerSecond(), 0, font.getLineHeight());
+        font.draw(spriteBatch, "FPS:" + Gdx.graphics.getFramesPerSecond(), 0, font.getLineHeight());
         spriteBatch.end();
     }
 
--- lwjgl3/src/main/java/io/github/c3team7/unisim/lwjgl3/Lwjgl3Launcher.java
+++ lwjgl3/src/main/java/io/github/c3team7/unisim/lwjgl3/Lwjgl3Launcher.java
@@ -20,10 +20,10 @@ public class Lwjgl3Launcher {
         configuration.setTitle("UniSim");
         //// Vsync limits the frames per second to what your hardware can display, and helps eliminate
         //// screen tearing. This setting doesn't always work on Linux, so the line after is a safeguard.
-        configuration.useVsync(true);
+        configuration.useVsync(false);
         //// Limits FPS to the refresh rate of the currently active monitor, plus 1 to try to match fractional
         //// refresh rates. The Vsync setting above should limit the actual FPS to match the monitor.
-        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        // configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
         //// If you remove the above line and set Vsync to false, you can get unlimited FPS, which can be
         //// useful for testing performance, but can also be very stressful to some hardware.
         //// You may also need to configure GPU drivers to fully disable Vsync; this can cause screen tearing.
